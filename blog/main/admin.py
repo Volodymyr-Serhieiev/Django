7c1434cf73f15a2e560b794ceae349414a2bbb03
SerhieievVolodymyr <vssergieiev@gmail.com>
2023-07-27 12:10:49 +0300

    HW 3

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, BlogPost, Author, Comment
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['headline', 'created_at']
+    filter_horizontal = ['authors']
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['headline', 'author', 'created_at']
+    filter_horizontal = ['topics']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'blog_post', 'created_at']
--- blog/main/models.py
+++ blog/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    headline = models.CharField(max_length=120)
+    created_at = models.DateField(auto_now=True)
+    authors = models.ManyToManyField('Author', blank=True)
+
+    def __str__(self):
+        return f'{self.headline}'
+
+
+class BlogPost(models.Model):
+    headline = models.CharField(max_length=120)
+    created_at = models.DateField(auto_now=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, blank=True, null=True)
+    topics = models.ManyToManyField(Topic)
+
+    def __str__(self):
+        return f'{self.headline}'
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, blank=True, null=True)
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
